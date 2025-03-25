4bb6c0c92bbe4acdf35b930fb5f29b581b916865
yashpoojari09 <yashpoojari009@gmail.com>
2025-03-25 19:08:33 +0530

    add Vercel json file

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+    res.send("Welcome to the API");
+});
