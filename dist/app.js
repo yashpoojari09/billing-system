02a9e0c94fbce5d58b4f6f6abe6783b41c36a46b
yashpoojari09 <yashpoojari009@gmail.com>
2025-03-27 10:36:46 +0530

    git

---
+++ dist/app.js
@@ -0,0 +1,46 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const passport_1 = __importDefault(require("passport"));
+const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
+require("./middlewares/auth"); // Initialize passport strategy
+// import customerRoutes from "./routes/customers";
+const tenants_1 = __importDefault(require("./routes/tenants"));
+const error_1 = require("./middlewares/error");
+// import inventoryRoutes from "./routes/inventory";
+// import taxationRoutes from "./routes/taxation";
+const logger_1 = __importDefault(require("./utils/logger"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+// Middleware
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use(passport_1.default.initialize());
+// Global Error Handler
+app.use(error_1.errorHandler);
+// Routes
+app.use("/api/auth", auth_routes_1.default);
+app.use("/api/tenants", tenants_1.default);
+// app.use("/api/customers", customerRoutes);
+// app.use("/api/inventory", inventoryRoutes);
+// app.use("/api/taxation", taxationRoutes);
+logger_1.default.info("Server is Starting...");
+// Health check route
+app.get("/", (_req, res) => {
+    res.send({ message: "Multi-Tenant Billing API is running" });
+});
+// Example logging route
+app.get("/test-log", (_req, res) => {
+    logger_1.default.info("📢 Test log from Vercel");
+    res.send({ message: "Log sent to Logtail" });
+});
+app.get("/test-env", (_req, res) => {
+    res.json({ TEST_SERVER_BILLING: process.env.TEST_SERVER_BILLING });
+});
+exports.default = app;
+//# sourceMappingURL=app.js.map
---
+++ dist/controllers/tenantController.js
@@ -0,0 +1,92 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteTenant = exports.updateTenant = exports.getTenantById = exports.getAllTenants = exports.createTenant = void 0;
+const client_1 = require("@prisma/client");
+const error_1 = require("../middlewares/error");
+const prisma = new client_1.PrismaClient();
+// CREATE Tenant (Only Superadmin)
+const createTenant = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { name, category } = req.body;
+        if (!name || !category) {
+            return next(new error_1.AppError("Name and category are required", 400));
+        }
+        const existingTenant = yield prisma.tenant.findUnique({ where: { name } });
+        if (existingTenant) {
+            return next(new error_1.AppError("Tenant with this name already exists", 400));
+        }
+        const tenant = yield prisma.tenant.create({
+            data: { name, category },
+        });
+        res.status(201).json(tenant);
+    }
+    catch (error) {
+        next(error);
+    }
+});
+exports.createTenant = createTenant;
+// GET All Tenants (Superadmin Only)
+const getAllTenants = (_req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const tenants = yield prisma.tenant.findMany();
+        res.json(tenants);
+    }
+    catch (error) {
+        next(error);
+    }
+});
+exports.getAllTenants = getAllTenants;
+// GET Tenant By ID (Admin & Superadmin)
+const getTenantById = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id: tenantId } = req.tenant;
+        const tenant = yield prisma.tenant.findUnique({ where: { id: tenantId } });
+        if (!tenantId) {
+            return next(new error_1.AppError("Tenant not found", 404));
+        }
+        res.json(tenant);
+    }
+    catch (error) {
+        next(error);
+    }
+});
+exports.getTenantById = getTenantById;
+// UPDATE Tenant (Admin & Superadmin)
+const updateTenant = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id: tenantId } = req.tenant;
+        const { name, category } = req.body;
+        const tenant = yield prisma.tenant.update({
+            where: { id: tenantId },
+            data: { name, category },
+        });
+        res.json(tenant);
+    }
+    catch (error) {
+        next(error);
+    }
+});
+exports.updateTenant = updateTenant;
+// DELETE Tenant (Only Superadmin)
+const deleteTenant = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id: tenantId } = req.tenant;
+        yield prisma.tenant.delete({ where: { id: tenantId } });
+        res.json({ message: "Tenant deleted successfully" });
+    }
+    catch (error) {
+        next(error);
+    }
+});
+exports.deleteTenant = deleteTenant;
+// Tenant
+//# sourceMappingURL=tenantController.js.map
